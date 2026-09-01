31d131ed8c2222c2930b07627089d9022070b853
leli1982 <133102486+leli1982@users.noreply.github.com>
2026-09-01 14:38:04 +0200

    feat: add staff sundries route

---
+++ app/staff/sundries/page.tsx
@@ -0,0 +1,5 @@
+import OperationalOrdersPage from "@/components/staff/OperationalOrdersPage";
+
+export default function StaffSundriesPage() {
+  return <OperationalOrdersPage orderType="sundries" />;
+}
