2cccf564633bad5e0dd9e24ea9dcbbdf3736643e
leli1982 <133102486+leli1982@users.noreply.github.com>
2026-09-01 14:11:56 +0200

    feat: add system users admin route

---
+++ app/bgm-admin/system-users/page.tsx
@@ -0,0 +1,7 @@
+import SystemUsersAdmin from "@/components/admin/SystemUsersAdmin";
+
+export const dynamic = "force-dynamic";
+
+export default function SystemUsersPage() {
+  return <SystemUsersAdmin />;
+}
