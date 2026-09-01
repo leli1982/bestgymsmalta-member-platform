d28b6b5d385c4c07ec8cc4d8653101b0e9f43efa
leli1982 <133102486+leli1982@users.noreply.github.com>
2026-09-01 14:17:27 +0200

    feat: add staff offline roster route

---
+++ app/staff/offline/page.tsx
@@ -0,0 +1,7 @@
+import OfflineRosterPage from "@/components/staff/OfflineRosterPage";
+
+export const dynamic = "force-dynamic";
+
+export default function StaffOfflinePage() {
+  return <OfflineRosterPage />;
+}
