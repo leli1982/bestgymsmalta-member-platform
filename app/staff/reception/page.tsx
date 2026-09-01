6b8f4f72e854cdc5e5f6b6e8eca618205499cbcf
leli1982 <133102486+leli1982@users.noreply.github.com>
2026-09-01 14:25:27 +0200

    feat: add NFC reception route

---
+++ app/staff/reception/page.tsx
@@ -0,0 +1,7 @@
+import NfcReceptionPage from "@/components/staff/NfcReceptionPage";
+
+export const dynamic = "force-dynamic";
+
+export default function StaffReceptionPage() {
+  return <NfcReceptionPage />;
+}
