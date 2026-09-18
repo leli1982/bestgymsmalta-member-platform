856dffb1f40ddc718c3c4e8f747a9be53bcaa936
leli1982 <133102486+leli1982@users.noreply.github.com>
2026-09-18 14:48:34 +0200

    Preserve town in direct staff review

--- components/staff/MembershipEnrollmentPage.tsx
+++ components/staff/MembershipEnrollmentPage.tsx
@@ -130,6 +130,7 @@ function StaffNewMembershipEnrollment({ memberNumber }: { memberNumber: string }
           lastName: participant.lastName,
           addressLine1: participant.addressLine1,
           addressLine2: participant.addressLine2,
+          town: participant.town,
           postcode: participant.postcode,
           idNumber: participant.idNumber,
           dateOfBirth: participant.dateOfBirth,
