e8c4449ea3b423af578f1b7a9b1f7ee1497868a8
leli1982 <133102486+leli1982@users.noreply.github.com>
2026-09-25 10:21:03 +0200

    Show shared Staff Portal return navigation throughout staff routes

--- app/staff/layout.tsx
+++ app/staff/layout.tsx
@@ -1,8 +1,10 @@
 import StaffGlobalScanner from "@/components/staff/StaffGlobalScanner";
+import StaffPortalReturnButton from "@/components/staff/StaffPortalReturnButton";
 
 export default function StaffLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <StaffPortalReturnButton />
       {children}
       <StaffGlobalScanner />
     </>
